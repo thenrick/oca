09861fe85caae4b05f962a1f5ab32f12ad7d8ba6
Tom Henricksen <tom.henricksen@gmail.com>
2016-06-23 12:43:23 -0500

    Create Problem10.java

---
+++ src/com/nationwide/oca/Problem10.java
@@ -0,0 +1,17 @@
+package com.nationwide.oca;
+
+class Problem10 {
+	public static void main(String[] args) {
+		new Problem10().pass();
+	}
+
+	public void pass() {
+		int a = 10, b = 20;
+		print(a);
+	}
+
+	public void print(int a) {
+		int c = b / a;
+		System.out.print(c);
+	}
+}
