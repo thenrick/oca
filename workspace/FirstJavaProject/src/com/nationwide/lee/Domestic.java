8a2ee7b90d87112601c02c32cdaefc6482ebdd96
Tom Henricksen <tom.henricksen@gmail.com>
2016-06-23 12:55:12 -0500

    remove

--- workspace/FirstJavaProject/src/com/nationwide/lee/Domestic.java
+++
@@ -1,6 +0,0 @@
-package com.nationwide.lee;
-
-public interface Domestic {
-
-    public void sayHello();
-}
--- workspace/FirstJavaProject/src/com/nationwide/lee/Wild.java
+++
@@ -1,9 +0,0 @@
-package com.nationwide.lee;
-
-public interface Wild {
-
-	public void quicklyHide();
-
-	public void eatWildGame();
-
-}
