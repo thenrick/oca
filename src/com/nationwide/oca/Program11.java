e3a78f4226de0fc269f06b25c65cad24bdcfaf85
Tom Henricksen <tom.henricksen@gmail.com>
2016-06-24 12:54:16 -0500

    Add next OCA question

---
+++ src/com/nationwide/oca/Program11.java
@@ -0,0 +1,13 @@
+package com.nationwide.oca;
+
+public class Program11 {
+	
+	    static int x = 10;  
+	    
+	    public static void main(String[] args) {
+	     for(int x=0;x<5;x++){
+	        }
+	      System.out.print(x);
+	    }
+
+}
